e7259ed899bd2742874a7afefff6e346208b9026
dhankar_infobot <infobot2016@gmail.com>
2021-01-02 11:17:48 +0530

    random synthetic data

---
+++ Data/test_code/random_data.py
@@ -0,0 +1,25 @@
+# generate some randomised data - Synthetic Dummy Data Pandas Dataframes
+import pandas as pd 
+import numpy as np 
+import random
+import time
+from datetime import datetime as dt_time
+from datetime import timedelta as tm_delta
+
+# df = pd.DataFrame(np.random.randint(33,16234,size=(10000, 8)), 
+# 		columns=['Col1','Col2','Col3','Col4','Col5','Col6','Col7','Col8'])
+# print(df.shape)
+# print(df.head(3))
+# print(df.tail(3))
+
+base_date = dt_time(2020, 2, 1) # 20200201 #YYYYMMDD # 1st FEB 2020
+
+dt_3_from_dt_base = base_date + random.random() * tm_delta(days=3)
+dt_4_from_dt_base = base_date + random.random() * tm_delta(days=4)
+dt_5_from_dt_base = base_date + random.random() * tm_delta(days=5)
+print(dt_3_from_dt_base, dt_4_from_dt_base, dt_5_from_dt_base)
+
+ls_rand_dt_tm = [ base_date + random.random() * tm_delta(days=x) for x in range(4)]
+print(ls_rand_dt_tm)
+
+#df.to_csv('df_random.csv')
