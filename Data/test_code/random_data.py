dbd18d1f2c00d5f3a75fffcbf613ceebec8d4c2e
dhankar_infobot <infobot2016@gmail.com>
2021-01-02 12:06:16 +0530

    ls_rand_dt_tm

--- Data/test_code/random_data.py
+++ Data/test_code/random_data.py
@@ -1,4 +1,4 @@
-# generate some randomised data - Synthetic Dummy Data Pandas Dataframes
+# generate randomised simulated data - Synthetic Dummy Data (Pandas Dataframes)
 import pandas as pd 
 import numpy as np 
 import random
@@ -14,12 +14,17 @@ from datetime import timedelta as tm_delta
 
 base_date = dt_time(2020, 2, 1) # 20200201 #YYYYMMDD # 1st FEB 2020
 
-dt_3_from_dt_base = base_date + random.random() * tm_delta(days=3)
-dt_4_from_dt_base = base_date + random.random() * tm_delta(days=4)
-dt_5_from_dt_base = base_date + random.random() * tm_delta(days=5)
-print(dt_3_from_dt_base, dt_4_from_dt_base, dt_5_from_dt_base)
+# dt_3_from_dt_base = base_date + random.random() * tm_delta(days=3)
+# dt_4_from_dt_base = base_date + random.random() * tm_delta(days=4)
+# dt_5_from_dt_base = base_date + random.random() * tm_delta(days=5)
+# print(dt_3_from_dt_base, dt_4_from_dt_base, dt_5_from_dt_base)
 
 ls_rand_dt_tm = [ base_date + random.random() * tm_delta(days=x) for x in range(4)]
-print(ls_rand_dt_tm)
+print(type(ls_rand_dt_tm[0])) #<class 'datetime.datetime'>
+dt_frmt = '%Y-%m-%d %H:%M:%S.%f'
+#dt_frmt = '%Y-%m-%d %H:%M:%S'
+print(str(ls_rand_dt_tm[2]))
+print(dt_time.strptime(str(ls_rand_dt_tm[2]),dt_frmt))
+#print(ls_rand_dt_tm)
 
 #df.to_csv('df_random.csv')
