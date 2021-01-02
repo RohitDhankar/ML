ebf23f671cc0989f334903bcb1c402e4b0566189
dhankar_infobot <infobot2016@gmail.com>
2021-01-02 13:20:41 +0530

    df['col_date_time'] = ls_rand_dt_delta

--- Data/test_code/random_data.py
+++ Data/test_code/random_data.py
@@ -9,8 +9,8 @@ from datetime import timedelta as tm_delta
 df = pd.DataFrame(np.random.randint(33,16234,size=(100000, 8)), 
 		columns=['Col1','Col2','Col3','Col4','Col5','Col6','Col7','Col8'])
 print(df.shape)
-print(df.head(2))
-print(df.tail(2))
+# print(df.head(2))
+# print(df.tail(2))
 
 base_date = dt_time(2020, 2, 1) # 20200201 #YYYYMMDD # 1st FEB 2020
 start_date = dt_time(2019, 2, 1)
@@ -21,22 +21,24 @@ end_date = dt_time(2020, 2, 1)
 # date_delta = (end_date - start_date) + start_date
 # print(date_delta)
 
-ls_rand_dt_delta = [ random.random() * (end_date - start_date) + start_date for x in range(10)]
-print(ls_rand_dt_delta)
-print(type(ls_rand_dt_delta[0])) #<class 'datetime.datetime'>
+ls_rand_dt_delta = [ random.random() * (end_date - start_date) + start_date for x in range(100000)]
+#print(ls_rand_dt_delta)
+#print(type(ls_rand_dt_delta[0])) #<class 'datetime.datetime'>
 dt_frmt = '%Y-%m-%d %H:%M:%S.%f'
 #dt_frmt = '%Y-%m-%d %H:%M:%S'
-print(str(ls_rand_dt_delta[2]))
-print(dt_time.strptime(str(ls_rand_dt_delta[2]),dt_frmt))
-
+# print(str(ls_rand_dt_delta[2]))
+# print(dt_time.strptime(str(ls_rand_dt_delta[2]),dt_frmt))
+df['col_date_time'] = ls_rand_dt_delta
+print(df.shape)
+print(df.head(2))
 
 # dt_3_from_dt_base = base_date + random.random() * tm_delta(days=3)
 # dt_4_from_dt_base = base_date + random.random() * tm_delta(days=4)
 # dt_5_from_dt_base = base_date + random.random() * tm_delta(days=5)
 # print(dt_3_from_dt_base, dt_4_from_dt_base, dt_5_from_dt_base)
 
-ls_rand_dt_tm = [ base_date + random.random() * tm_delta(days=x) for x in range(100000)]
-df['col_date_time'] = ls_rand_dt_tm
+#ls_rand_dt_tm = [ base_date + random.random() * tm_delta(days=x) for x in range(100000)]
+#df['col_date_time'] = ls_rand_dt_tm
 # print(df.shape)
 # print(df.head(2))
 # print(df.tail(2))
