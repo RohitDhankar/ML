d62daecdaf7b61a936c96de5bd677f2c55d1a6ea
dhankar_infobot <infobot2016@gmail.com>
2021-01-04 09:18:56 +0530

    ALTER PROC spGmailParams

--- Data/test_code/sql_queries/proc_spGmailParams.sql
+++ Data/test_code/sql_queries/proc_spGmailParams.sql
@@ -1,10 +1,14 @@
 -- execute spGmailParams 'gmail.com' , 'gmail.com'
-execute spGmailParams 'gmail.com'
+execute spGmailParams1 'gmail.com' , 8
+-- DROP PROC spGmailParams1
 
-CREATE PROC spGmailParams
-@param_email nvarchar(20)
+-- CREATE PROC spGmailParams1
+-- ALTER PROC spGmailParams1
+@param_email nvarchar(20),
 -- @param_concat_cols nvarchar(20)
+@param_int int
 AS
 BEGIN
-	SELECT concat_cols FROM df_unq_df_random_1 WHERE RIGHT(concat_cols,9) = @param_email;
+	SELECT concat_cols , Col8 FROM df_unq_df_random_1 
+	WHERE RIGHT(concat_cols,9) = @param_email AND RIGHT(Col8,1) = @param_int
 END
